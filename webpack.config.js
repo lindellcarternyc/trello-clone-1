5baac95f8a54aebb8bf2fe04d3f7351e9c8b8e01
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-20 14:52:49 -0500

    Updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const webpack = require('webpack')
+const path = require('path')
 
+const src = path.join(__dirname, 'src')
 const config = {
   devServer: {
     hot: true,
     inline: true,
   },
+  entry: {
+    index: path.join(src, 'index.html')
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js'
+  }
 }
 
 module.exports = config
